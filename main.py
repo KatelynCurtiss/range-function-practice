4f0d3f396e7840dc8794e1c359052bbb9768af30
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-11-01 10:39:27 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# Katelyn Curtiss
+# November 1 2024
+# Range Function Practice
+
+
+# 4-3. Counting to twenty
+numbers = list(range(1 , 20))
+for n in numbers:
+  print(n)
+
+
+# 4-4. One million
+onemillion = range(1 , 1000000000)
+for n in onemillion:
+    print(n)
+
+# 4-5. Summing a million
+million = range(1 , 1000000000)
+min(million)
+max(million)
+sum(million)
+for i in million:
+    print(i)
+
+
+# 4-6. Odd numbers 
+odd_numbers = range(1, 12, 1)
+for x in odd_numbers:
+  print(x)
+
+# 4-7. Threes
+multiple_threes = range(3,30)
+for x in multiple_threes:
+   print(x)
+
+
+# 4-8. Cubes
+cubes = [value**2 for value in range(1,10) ]
+for c in cubes:
+   print(c)
+
+
+
